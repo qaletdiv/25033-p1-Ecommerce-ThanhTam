0a6f00a4951493a7f170aed23cf8c039a08b2ccf
judenns <ludenstran@gmail.com>
2025-10-28 05:46:48 +0700

    @feat: - update product card ui buttons- update section spacing- update buttons, input sizes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
-import { createIcons, Search, ShoppingCart, User } from "lucide";
+import { createIcons, Search, ShoppingCart, User, ChevronRight } from "lucide";
 import { products } from "./mock-data.js";
-
-createIcons({
-	icons: {
-		Search,
-		ShoppingCart,
-		User,
-	},
-});
 
 localStorage.setItem("productList", JSON.stringify(products));
 
@@ -18,13 +10,25 @@ container.innerHTML = productList
 	.map(
 		(product) => `
 		<div class="product-card">
-			<img class="product-img" src="${product.images[0].url}">
+			<a class="product-img"  href="#"><img src="${product.images[0].url}"></a>
 			<div class="product-info">
-				<a class="product-category">${product.category}</a>
+				<a href="#" class="product-category">${product.category}</a>
 				<p class="product-name">${product.name}</p>
 				<p class="product-price">${product.price}</p>
 			</div>
-			<button class="btn btn--primary">Xem ngay</button>
+			<div class="btn-group">
+				<button class="btn btn--primary">Thêm vào giỏ</button>
+				<a href="#">Xem chi tiết <i data-lucide="chevron-right" width="16" height="16"></i></a>
+			</div>
 		</div>`,
 	)
 	.join("");
+
+createIcons({
+	icons: {
+		Search,
+		ShoppingCart,
+		User,
+		ChevronRight,
+	},
+});
